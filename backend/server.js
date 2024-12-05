77fb505531b20ed517f948fccb4d951d062e5432
ANSH <puniaansh29@gmail.com>
2024-12-06 03:39:57 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running');
+});
